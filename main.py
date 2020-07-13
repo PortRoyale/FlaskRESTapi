4e02b978f064116af4a2fe8817c9630654456431
Tyler Casper <casperw@colorado.edu>
2020-07-13 11:15:57 -0500

    Finished the Flask REST api tutorial. Learning progressed.

--- main.py
+++ main.py
@@ -1,26 +1,94 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app) # wrapping our app in an api
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # will make and save a db file in current directory
+db = SQLAlchemy(app)
 
-video_put_args = reqparse.RequestParser() # make new RequestParser object
+class VideoModel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False) # 100 max characters and no empty names
+	views = db.Column(db.Integer, nullable=False)
+	likes = db.Column(db.Integer, nullable=False)
+
+	def __repr__(self):
+		return f"Video(name={name}, views={views}, likes={likes})" # f strings print/insert what is in brackets
+
+
+######
+# db.create_all() # DELETE THIS OR COMMENT IT OUT AFTER INITITIAL DB CREATION!!!! ONLY ONE TIME SHOULD IT BE RUN OR WILL OVERWRITE DATABASE
+
+
 
 # RequestParser
-video_put_args.add_argument("name", type=str, help="Name of video is required", required=True)
-video_put_args.add_argument("views", type=int, help="Views of video are required", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes of video are required", required=True)
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-videos = {}
+# for Video.patch(video_id) to update only 'views' for example
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
-class Video(Resource): # HelloWorld is a Resource class, we are inheriting
+
+resource_fields = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'views': fields.Integer,
+	'likes': fields.Integer
+}
+
+class Video(Resource): # Resource class, we are inheriting
+	@marshal_with(resource_fields) # DECORATOR: this serializes every instance into a json format
 	def get(self, video_id): # overriding the get method
-		return videos[video_id] # need key/value pair (HAS TO BE SERIALIZABLE!)
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that ID.")
+		return result # need key/value pair (HAS TO BE SERIALIZABLE!)
 
+	@marshal_with(resource_fields)
 	def put(self, video_id):
 		args = video_put_args.parse_args()
-		videos[video_id] = args
-		return videos[video_id], 201 # STATUS CODE 201='created'
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if result:
+			abort(409, message=f"Video ID {video_id} in database.")
+
+		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+		db.session.add(video)
+		db.session.commit()
+		return video, 201
+
+	@marshal_with(resource_fields)
+	def patch(self, video_id):
+		args = video_update_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Video doesn't exist, cannot update.")
+
+		if args['name']:
+			result.name = args['name']
+		if args['views']:
+			result.views = args['views']
+		if args['likes']:
+			result.likes = args['likes']
+
+		db.session.commit()
+
+		return result
+
+	def delete(self, video_id):
+		abort_if_video_id_doesnt_exist(video_id)
+		del videos[video_id]
+		return '', 204 # "Hey, it worked. Youw ere able to delete it."
+
+	# def get_all(self):
+	# 	for video_id in videos:
+	# 		print(video_id)
+
 
 api.add_resource(Video, "/video/<int:video_id>") # adding HelloWorld resource to the api. "/" is default, but if wanted reponse when user typed "HelloWorld" we would make it "/HelloWorld"
 
--- test.py
+++ test.py
@@ -3,8 +3,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes":10, "name":"Freedom Show", "views":990}) # sending helloworld because we added the resource to server of "helloworld"
-print(response.json()) # need to decode it to readable format
-input() # pause cmd line
-response = requests.get(BASE + "video/1") # sending helloworld because we added the resource to server of "helloworld"
+
+data = [{'likes':10, 'name':'Freedom Show', 'views':990},
+		{"likes":56, "name":"Free Cheese", "views":0},
+		{"likes":6969, "name":"Your mommas tapes", "views":69}]
+
+for i in range(len(data)):
+	response = requests.put(BASE + "video/" + str(i), data[i])
+	print(response.json())
+
+input()
+response = requests.get(BASE + "video/2") # sending helloworld because we added the resource to server of "helloworld"
 print(response.json()) # need to decode it to readable format
